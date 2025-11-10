b9afba5ac099cbe0224fd3454e2af990e10e63cc
kitkack <souler2580@foxmail.com>
2025-11-11 00:10:29 +0800

    refactor: 合并组件，面板高度视觉统一

--- src/entrypoints/newtab/components/settings/SettingHall.tsx
+++ src/entrypoints/newtab/components/settings/SettingHall.tsx
@@ -3,10 +3,8 @@ import { GeneralPanel } from "./panels/GenerialPanel";
 import { SearchPredictionPanel } from "./panels/SearchPrediction";
 import { IconHelperPanel } from "./panels/IcoHelperPanel";
 import { SearchEnginePanel } from "./panels/SearchEngine";
-import { ThemePanel } from "./panels/ThemePanel";
 
 const panels = [
-  ThemePanel,
   GeneralPanel,
   SearchEnginePanel,
   SearchPredictionPanel,
